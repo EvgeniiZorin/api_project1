5a805e6760c6af683bb4778124cf06282e5e2e48
Evgenii Zorin <zorin.evgenii.m@gmail.com>
2023-03-20 14:51:54 -0600

    v 3 | added functionality - access random quotes; added quote csv files

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
 from datetime import datetime
+import random
+import os
+import pandas as pd
 
 app = FastAPI()
 
-posts = {
-    "post1": "message1", 
-    "post2": "message2"
+quotes = {
+    "1": "Text for quote 1", 
+    "2": "Text for quote 2",
+    "3": "Text for quote 3",
+    "4": "Text for quote 4",
+    "5": "Text for quote 5"
 }
+
+# df_quotes_mixed = pd.read_csv('quotes_mixed.csv')
+df_quotes_mixed_path = os.path.abspath( os.path.join((os.path.dirname( __file__ )), 'quotes_mixed.csv') )
+df_quotes_mixed = pd.read_csv(df_quotes_mixed_path)
+
 
 @app.get('/')
 def read_root():
     return {"welcome_message": "welcome to my API!"}
 
-@app.get('/posts')
-def get_posts():
-    return posts
+@app.get('/all_quotes')
+def get_quotes():
+    return df_quotes_mixed.values.tolist()
+
+@app.get('/random_quote')
+def get_random_quote():
+    random_quote = df_quotes_mixed.sample(1)
+    author = random_quote['Author'].item()
+    quote_text = random_quote['Quote'].item()
+    return { author: quote_text }
